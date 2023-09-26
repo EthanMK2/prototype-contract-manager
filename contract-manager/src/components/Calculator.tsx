95f4be8bc0fcac13df4b62d8c8dd7e560627b9da
EthanMK2 <ethan7klukkert@gmail.com>
2023-09-26 11:20:57 -0400

    none

--- contract-manager/src/components/Calculator.tsx
+++ contract-manager/src/components/Calculator.tsx
@@ -1,253 +1,59 @@
 import { useState } from "react";
 
-const performOperation: any = {
-  "+": function (x: number, y: number): number {
-    return x + y;
-  },
-  "-": function (x: number, y: number): number {
-    return x - y;
-  },
-  x: function (x: number, y: number): number {
-    return x * y;
-  },
-  "/": function (x: number, y: number): number {
-    return x / y;
-  },
-};
+// const performOperation: any = {
+//   "+": function (x: number, y: number): number {
+//     return x + y;
+//   },
+//   "-": function (x: number, y: number): number {
+//     return x - y;
+//   },
+//   x: function (x: number, y: number): number {
+//     return x * y;
+//   },
+//   "/": function (x: number, y: number): number {
+//     return x / y;
+//   },
+// };
+
+//////////////////////////////// TODO: MAKE SOLUTION ON WHITE BOARD, THEN IMPLEMENT (make string from values, prevent certain entries when ending is specific operators, pass to final analysis function)
 
 const Calculator = () => {
-  const numEntered = (num: number) => {
-    setCurNum((prevNumber) => {
-      if (!prevNumber && !operation) {
-        setResult(num.toString());
-        return num;
-      } else if (prevNumber && !operation) {
-        setResult((prevResult) => {
-          if (num === 0 && prevNumber.toString().includes(".")) {
-            const trailZeroString: string = prevResult.toString() + num.toString();
-            return parseFloat(trailZeroString).toFixed(trailZeroString.split('.')[1].length);
-          }
-          return parseFloat(prevResult.toString() + num.toString()).toString();
-        });
-        return parseFloat(prevNumber.toString() + num.toString());
-      } else if (prevNumber && operation) {
-        return parseFloat(prevNumber.toString() + num.toString());
-      } else if (!prevNumber && operation) {
-        return num;
-      }
-
-      // will never run?
-      return num;
-    });
-    setDisplayedOperations(() => {
-      let displayOperation: string;
-      if (curNum && !operation) {
-        return (displayOperation =
-          `${curNum.toString()}` + `${num.toString()}`);
-      } else if (curNum && operation) {
-        displayOperation = `${curNum.toString()}` + `${num.toString()}`;
-        return displayOperation;
-      } else {
-        return num.toString();
-      }
-    });
-  };
-  const operationEntered = (operation: string) => {
-    // show result when previous operation exists
-    setOperation((prevOperation) => {
-      if (operation === "." && !prevOperation) {
-        console.log("Make Decimal Function");  // prime number for first number after decimal? CHECK IF "." already there (already a decimal)
-        return null;
-      } else if (operation === "." && prevOperation != ".") {
-        console.log("don't touch previous operation: do nothing");
-        return prevOperation;
-      } else if (operation === "." && prevOperation == ".") {
-        console.log("repeated decimal: do nothing")
-        // track whether curNum is already a decimal?
-        return prevOperation;
-      }
-      if (prevOperation && curNum) {
-        setResult((prevResult) => {
-          return performOperation[prevOperation](prevResult, curNum).toString();
-        });
-        return operation;
-      } else {
-        if (curNum) {
-          setDisplayedOperations(`${curNum}${operation}`);
-          return operation;
-        } else if (prevOperation) {
-          setDisplayedOperations((prevDisplay) => {
-            const newOperationDisplay =
-              prevDisplay!.slice(0, prevDisplay!.length - 1) + operation;
-            return newOperationDisplay;
-          });
-          return operation;
-        } else {
-          return null;
-        }
-      }
-    });
-    if (operation === ".") {
-      return;
-    } else if (curNum && operation) {
-      setCurNum(null);
-    }
-  };
-
-  const clearNumbers = () => {
-    setCurNum(null);
-    setResult("");
-    setOperation(null);
-    setDisplayedOperations(null);
-  };
-
-  const onEquals = () => {
-    if (curNum && operation) {
-      setResult((prevResult) => {
-        const result: number = performOperation[operation](prevResult, curNum);
-        setCurNum(result);
-        setDisplayedOperations(result.toString());
-        return result.toString();
-      });
-      setOperation(null);
-    }
-  };
-
-  const [displayedOperations, setDisplayedOperations] = useState<string | null>(
-    null
-  );
-  const [curNum, setCurNum] = useState<number | null | undefined>(0);
-  const [operation, setOperation] = useState<string | null>(null);
-  const [result, setResult] = useState<string>("");
+  // const [curNum, setCurNum] = useState<number | null | undefined>(0);
+  // const [operation, setOperation] = useState<string | null>(null);
+  // const [result, setResult] = useState<string>("");
 
   return (
     <div>
-      <h1>{displayedOperations}</h1>
-      <h2>= {result}</h2>
+      <h1>{}</h1>
+      <h2>= {}</h2>
       <ul>
         <div>
-          <button
-            onClick={() => {
-              numEntered(0);
-            }}
-          >
-            0
-          </button>
-          <button
-            onClick={() => {
-              numEntered(1);
-            }}
-          >
-            1
-          </button>
-          <button
-            onClick={() => {
-              numEntered(2);
-            }}
-          >
-            2
-          </button>
+          <button onClick={() => {}}>0</button>
+          <button onClick={() => {}}>1</button>
+          <button onClick={() => {}}>2</button>
         </div>
         <div>
-          <button
-            onClick={() => {
-              numEntered(3);
-            }}
-          >
-            3
-          </button>
-          <button
-            onClick={() => {
-              numEntered(4);
-            }}
-          >
-            4
-          </button>
-          <button
-            onClick={() => {
-              numEntered(5);
-            }}
-          >
-            5
-          </button>
+          <button onClick={() => {}}>3</button>
+          <button onClick={() => {}}>4</button>
+          <button onClick={() => {}}>5</button>
         </div>
         <div>
-          <button
-            onClick={() => {
-              numEntered(6);
-            }}
-          >
-            6
-          </button>
-          <button
-            onClick={() => {
-              numEntered(7);
-            }}
-          >
-            7
-          </button>
-          <button
-            onClick={() => {
-              numEntered(8);
-            }}
-          >
-            8
-          </button>
+          <button onClick={() => {}}>6</button>
+          <button onClick={() => {}}>7</button>
+          <button onClick={() => {}}>8</button>
         </div>
         <div>
-          <button
-            onClick={() => {
-              numEntered(9);
-            }}
-          >
-            9
-          </button>
-          <button
-            onClick={() => {
-              operationEntered("-");
-            }}
-          >
-            -
-          </button>
-          <button
-            onClick={() => {
-              operationEntered("+");
-            }}
-          >
-            +
-          </button>
+          <button onClick={() => {}}>9</button>
+          <button onClick={() => {}}>-</button>
+          <button onClick={() => {}}>+</button>
         </div>
         <div>
-          <button
-            onClick={() => {
-              operationEntered("x");
-            }}
-          >
-            x
-          </button>
-          <button
-            onClick={() => {
-              operationEntered("/");
-            }}
-          >
-            ÷
-          </button>
-          <button
-            onClick={() => {
-              operationEntered(".");
-            }}
-          >
-            .
-          </button>
+          <button onClick={() => {}}>x</button>
+          <button onClick={() => {}}>÷</button>
+          <button onClick={() => {}}>.</button>
         </div>
-        <button
-          onClick={() => {
-            clearNumbers();
-          }}
-        >
-          C
-        </button>
-        <button onClick={onEquals}>=</button>
+        <button onClick={() => {}}>C</button>
+        <button onClick={() => {}}>=</button>
       </ul>
     </div>
   );
